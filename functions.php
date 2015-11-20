36f00aba3cae764075352637de9fe9ebb2f24deb
Kharis Sulistiyono <kharisblank@gmail.com>
2015-11-21 05:09:44 +0700

    Zerif Lite child theme

--- functions.php
+++ functions.php
@@ -9,3 +9,14 @@ if ( ! function_exists( 'child_theme_enqueue_styles' ) ) :
 
   }
 endif;
+
+
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+         update_option( 'theme_mods_' . get_template(), $value );
+         return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
